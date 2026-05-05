88d942ce509f5f5a7f053e8a521b10d4c5a13f93
Shafa Khoirunnisa <shafaafiya46@gmail.com>
2026-05-06 00:23:59 +0700

    progress lokal

--- simpan.php
+++ simpan.php
@@ -13,14 +13,10 @@ $no = $_POST['no_kk'];
 $nama = $_POST['nama_kepala_keluarga'];
 $alamat = $_POST['alamat'];
 
-$simpanKK = mysqli_query($koneksi, "INSERT INTO kartu_keluarga 
+mysqli_query($koneksi, "INSERT INTO kartu_keluarga 
 (user_id, no_kk, nama_kepala_keluarga, alamat)
 VALUES
 ('$user_id', '$no', '$nama', '$alamat')");
-
-if (!$simpanKK) {
-    die("Gagal simpan data KK: " . mysqli_error($koneksi));
-}
 
 $id = mysqli_insert_id($koneksi);
 
@@ -30,16 +26,11 @@ $jk = $_POST['jenis_kelamin'];
 $hub = $_POST['hubungan'];
 
 for ($i = 0; $i < count($nik); $i++) {
-    $simpanAnggota = mysqli_query($koneksi, "INSERT INTO anggota_keluarga
+    mysqli_query($koneksi, "INSERT INTO anggota_keluarga
     (id_kk, nik, nama, jenis_kelamin, hubungan)
     VALUES
     ('$id', '$nik[$i]', '$namaA[$i]', '$jk[$i]', '$hub[$i]')");
-
-    if (!$simpanAnggota) {
-        die("Gagal simpan anggota: " . mysqli_error($koneksi));
-    }
 }
 
 header("Location: landing.php?id=$id");
 exit;
-?>  
